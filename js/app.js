3e8a8aa57d4b359d1971130e8bdbed00e050f844
ahmed rad <ahmedrad@Ahmeds-MacBook-Air.local>
2015-01-05 00:10:37 -0800

    more code

--- js/app.js
+++ js/app.js
@@ -3,15 +3,17 @@ var incompleteTasksHolder = document.getElementById('incomplete-tasks');
 var completedTasksHolder = document.getElementById('completed-tasks');
 
 var addTaskAction = document.getElementsByTagName('button')[0];
-var editTaskActions = document.getElementsByClassName('edit');
-var deleteTaskActions = document.getElementsByClassName('delete');
-var checkBoxActions = document.querySelectorAll('input[type="checkbox"]');
-
-
 
 
 var addTask = function() {
   console.log('adding task...')
+  var newTaskName = document.getElementById('new-task').value;
+  if (newTaskName === '')
+    {return;}
+  var newTask = '<li><input type="checkbox"><label>'+newTaskName+'</label><input type="text"><button class="edit">Edit</button><button class="delete">Delete</button></li>';
+  incompleteTasksHolder.insertAdjacentHTML('beforeend', newTask);
+  wireActions();
+  console.log('added Task')
 };
 
 var editTask = function() {
@@ -21,20 +23,29 @@ var editTask = function() {
 
 var deleteTask = function() {
   console.log('deleting task...')
+  var task = this.parentNode;
+  console.log(task);
+  var taskParent = task.parentNode;
+  console.log(taskParent);
+  taskParent.removeChild(task);
+  wireActions();
 }
 
 var checkBoxHandler = function() {
   console.log('handle checkbox');
-  console.log(this);
+  var task = this.parentNode;
+  var taskParent = task.parentNode;
+  if (taskParent.id === 'completed-tasks')
+    {
+      incompleteTasksHolder.appendChild(task);
+    }
+  else
+    {
+      completedTasksHolder.appendChild(task);
+    }
+
 }
 
-
-console.log(incompleteTasksHolder);
-console.log(completedTasksHolder);
-console.log(addTaskAction);
-console.log(editTaskActions);
-console.log(deleteTaskActions);
-console.log(checkBoxActions);
 
 addTaskAction.onclick = addTask;
 
@@ -48,20 +59,37 @@ addTaskAction.onclick = addTask;
 // }
 
 // Note ellision, there is no member at 2 so it isn't visited
-for(i=0; i<editTaskActions.length;i++)
-{
-  editTaskActions[i].onclick = editTask;
-}
+ 
+var wireActions = function() {
 
-for(i=0; i<deleteTaskActions.length;i++)
-{
-  deleteTaskActions[i].onclick = deleteTask;
-}
+  var editTaskActions = document.getElementsByClassName('edit');
+  var deleteTaskActions = document.getElementsByClassName('delete');
+  var checkBoxActions = document.querySelectorAll('input[type="checkbox"]');
 
-for(i=0; i<deleteTaskActions.length;i++)
-{
-  checkBoxActions[i].onchange = checkBoxHandler;
-}
+  for(i=0; i<editTaskActions.length;i++)
+  {
+    editTaskActions[i].onclick = editTask;
+  }
+
+  for(i=0; i<deleteTaskActions.length;i++)
+  {
+    deleteTaskActions[i].onclick = deleteTask;
+  }
+
+  for(i=0; i<deleteTaskActions.length;i++)
+  {
+    checkBoxActions[i].onchange = checkBoxHandler;
+  }
+
+  console.log(incompleteTasksHolder);
+  console.log(completedTasksHolder);
+  console.log(addTaskAction);
+  console.log(editTaskActions);
+  console.log(deleteTaskActions);
+  console.log(checkBoxActions);
+};
+
+wireActions();
 
 
 
